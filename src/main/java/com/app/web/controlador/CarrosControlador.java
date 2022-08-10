ec57ec77e1e4250362344a15ffaaf7b0741be1c0
Rolando Duverge <arturo1872002@gmail.com>
2022-08-10 12:41:59 -0400

    add selenium

--- src/main/java/com/app/web/controlador/CarrosControlador.java
+++ src/main/java/com/app/web/controlador/CarrosControlador.java
@@ -1,17 +1,17 @@
 package com.app.web.controlador;
 
 import com.app.web.servicio.CarrosServicio;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
+
 import com.app.web.entidad.Carros;
 import com.app.web.excel.ExcelExporter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -32,11 +32,10 @@ import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import com.lowagie.text.DocumentException;
+import testselenium.DriverFactory;
 
 
 @Controller
@@ -105,7 +104,7 @@ public class CarrosControlador {
         excelExporter.export(response);
     }
 
-    @GetMapping("/users/export/pdf")
+    @GetMapping("/carros/export/pdf")
     public void exportToPDF(HttpServletResponse response) throws DocumentException, IOException {
         response.setContentType("application/pdf");
         DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
@@ -119,6 +118,45 @@ public class CarrosControlador {
 
         PdfExporter exporter = new PdfExporter(listaCarros);
         exporter.export(response);
+    }
+    @GetMapping("/users/data/import")
+    public void DatagettingFromExcel(HttpServletResponse response) throws DocumentException, IOException {
+        WebDriver driver=DriverFactory.getDriverFor("chrome");
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("http://localhost:8090/");
+        FileInputStream fis=new FileInputStream("C:\\Users\\Rolando\\Desktop\\SpringBoot-Crud-Cars-main\\src\\excel\\CarrosForm.xlsx");
+        XSSFWorkbook workbook=new XSSFWorkbook(fis);
+        XSSFSheet sheet=workbook.getSheet("sheet");
+        int rowcount =sheet.getLastRowNum();
+        int colcount=sheet.getRow(1).getLastCellNum();
+        System.out.println("rowcount :"+rowcount+" colcount : "+colcount);
+        for (int i = 1; i < rowcount; i++) {
+            XSSFRow celldata=sheet.getRow(i);
 
+            String marca=celldata.getCell(0).getStringCellValue();
+            String modelo= ((XSSFRow) celldata).getCell(1).getStringCellValue();
+            String descripcion=celldata.getCell(2).getStringCellValue();
+            String tipo_Combustible=celldata.getCell(3).getStringCellValue();
+            String numero_de_Chasis=celldata.getCell(4).getStringCellValue();
+            String numero_de_Placa=celldata.getCell(5).getStringCellValue();
+            String tipo_de_vehiculo=celldata.getCell(6).getStringCellValue();
+
+            driver.findElement(By.cssSelector("#marca")).sendKeys(marca);
+            driver.findElement(By.cssSelector("#marca")).clear();
+            driver.findElement(By.cssSelector("#modelo")).sendKeys(modelo);
+            driver.findElement(By.cssSelector("#modelo")).clear();
+            driver.findElement(By.cssSelector("#descripcion")).sendKeys(descripcion);
+            driver.findElement(By.cssSelector("#descripcion")).clear();
+            driver.findElement(By.cssSelector("#tipo_Combustible")).sendKeys(tipo_Combustible);
+            driver.findElement(By.cssSelector("#tipo_Combustible")).clear();
+            driver.findElement(By.cssSelector("#numero_de_Chasis")).sendKeys(numero_de_Chasis);
+            driver.findElement(By.cssSelector("#numero_de_Chasis")).clear();
+            driver.findElement(By.cssSelector("#numero_de_Placa")).sendKeys(numero_de_Placa);
+            driver.findElement(By.cssSelector("#numero_de_Placa")).clear();
+            driver.findElement(By.cssSelector("#tipo_de_vehiculo")).sendKeys(tipo_de_vehiculo);
+            driver.findElement(By.cssSelector("#tipo_de_vehiculo")).clear();
+
+            System.out.println(i+"."+marca+" // " +modelo+" // "+descripcion+" // "+tipo_Combustible+" // "+numero_de_Chasis+" // "+numero_de_Placa+" // "+tipo_de_vehiculo);
+        }
     }
 }
